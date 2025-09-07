198366dc54f601c564e66859b582bb63e47ff27a
Johny Samuael <johnnysamuael@gmail.com>
2025-09-07 16:56:35 +0000

    Can you make this the photo ?

--- src/components/chest.tsx
+++ src/components/chest.tsx
@@ -1,16 +1,15 @@
-
 import Image from 'next/image';
 
 export function Chest() {
   return (
     <div className="relative w-48 h-48 md:w-64 md:h-64">
       <Image
-        src="https://picsum.photos/256/256"
-        alt="A mysterious chest"
+        src="https://i.imgur.com/gB32r8Q.png"
+        alt="Clash 6900 Coin Logo"
         width={256}
         height={256}
         className="object-contain"
-        data-ai-hint="clash royale chest"
+        data-ai-hint="clash royale coin"
       />
     </div>
   );
