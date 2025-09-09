8bb9195b778289abe64b587ccd07633b159b903f
Johny Samuael <johnnysamuael@gmail.com>
2025-09-09 16:39:18 +0000

    Can you use this video for the landing page instead/ its a 16;9 aspect r

--- src/components/chest.tsx
+++ src/components/chest.tsx
@@ -2,7 +2,7 @@ export function Chest() {
   return (
     <div className="relative w-48 h-48 md:w-64 md:h-64">
       <video
-        src="/videos/clash.mp4"
+        src="/videos/clash2.mp4"
         autoPlay
         loop
         muted
